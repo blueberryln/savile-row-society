41b8b4ce2b3cf70a56befa33f5275f1228812451
Saurabh Sharma <saurabh@mobikasa.com>
2013-10-30 18:30:52 +0530

    Change stylist email

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -940,7 +940,7 @@ class UsersController extends AppController {
                         $email->subject('Savile Row Stylist: Your stylist!');
                         $email->template('user_stylist');
                         $email->emailFormat('html');
-                        $email->viewVars(compact('name','stylist_name'));
+                        $email->viewVars(array('name' => $name, 'stylist_name' => $stylist_name));
                         $email->send();    
                     }
                     catch(Exception $e){
