d6fefd7a9d6185e20c0be6efeef128adc9903b40
Saurabh Sharma <saurabh@mobikasa.com>
2013-11-05 16:58:25 +0530

    Changes for checkout debugging

--- app/Controller/ClosetController.php
+++ app/Controller/ClosetController.php
@@ -16,12 +16,12 @@ class ClosetController extends AppController {
     function beforeFilter() {
         if(!$this->request->is('ssl')){ 
             if($this->request->params['action'] == "checkout" || $this->request->params['action'] == "validatecard"){
-                $this->redirect('https:' . env('SERVER_NAME') . $this->here, null, true);
+                $this->redirect('https:' . env('SERVER_NAME') . $this->here);
             }
         } 
         else{
             if($this->request->params['action'] != "checkout" && $this->request->params['action'] != "validatecard"){
-                $this->redirect('http:' . env('SERVER_NAME') . $this->here, null, true);
+                $this->redirect('http:' . env('SERVER_NAME') . $this->here);
             }
         }
     }
