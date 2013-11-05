6a58ddf476d159a1d0ca129220fd5f2cddb070eb
Saurabh Sharma <saurabh@mobikasa.com>
2013-11-05 18:04:25 +0530

    Changes for checkout debugging

--- app/Controller/ClosetController.php
+++ app/Controller/ClosetController.php
@@ -496,6 +496,9 @@ class ClosetController extends AppController {
             $error_cart = false;
             $error = false;
             
+            print_r($this->request->data);
+            exit;
+            
             $request_data = $this->request->data['billing'];
             
             //Arrange Billing data
@@ -720,9 +723,6 @@ class ClosetController extends AppController {
     }
     
     public function updateBillingAddress($data, $user_id){
-        print_r($data);
-        exit;
-        
         $BillingAddress = ClassRegistry::init('BillingAddress');
         $address = $BillingAddress->getByUserID($user_id);
         $data['BillingAddress']['user_id'] = $user_id;
