aabe94817a582d610ab20c9eef2dfb20ac433089
shubham goel <shubhamgoel@mobikasa.com>
2015-04-29 19:19:55 +0530

    fb login test

--- app/Controller/ConnectController.php
+++ app/Controller/ConnectController.php
@@ -111,6 +111,7 @@ class ConnectController extends AppController {
                         $email->emailFormat('html');
                         $email->viewVars(array('name' => $profile['first_name']));
                         $email->send();*/
+                        echo '<pre>';print_r($fb_data);die;
                         App::import('Controller', 'Users');
                         $Users = new UsersController;
                         $stylist_id = $Users->assign_refer_stylist($fb_data['User']['id']);
@@ -118,8 +119,6 @@ class ConnectController extends AppController {
                         $Messages = new MessagesController;
                         $Messages->send_welcome_message($fb_data['User']['id'], $stylist_id);
                         $this->mailto_sales_team($fb_data,$stylist_id);    // sends an email to the sales team
-                        echo '<pre>';print_r($Users);
-                        print_r($Messages);die;
                         // redirect to home
                         //$this->Session->setFlash(__('Your account is created with your Facebook data.'), 'modal', array('class' => 'success', 'title' => 'Hooray!'));
                         //$this->redirect('/');
