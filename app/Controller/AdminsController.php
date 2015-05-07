1c245ce2a9883d03e1c990f922eb9a74ed804237
saurabh <saurabh@mobikasa.com>
2015-05-07 22:02:55 +0530

    remove debug

--- app/Controller/AdminsController.php
+++ app/Controller/AdminsController.php
@@ -148,9 +148,6 @@ class AdminsController extends AppController {
 		$this->loadModel('Blog');
 		$this->layout = 'adminlte';
 		$data = $this->data;
-
-		print_r($data);
-		exit;
 		$this->set(compact('data'));
 		if(!empty($data)){
 			$data['Blog']['time'] = time();
